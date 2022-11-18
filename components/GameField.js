391ca0c08dc5893e58fdd2f8471c99a486c06dd0
cw118 <carolyn94118@gmail.com>
2022-11-17 20:34:30 -0500

    Fix synchronous script and clear readme

--- components/GameField.js
+++ components/GameField.js
@@ -1,9 +1,10 @@
 // import styles from '../styles/<>.module.css';
+import Script from 'next/script'
 
 const GameField = () => {
   return (
     <>
-      <script type="module" src="https://unpkg.com/@google/model-viewer/dist/model-viewer.min.js"></script>
+      <Script type="module" src="https://unpkg.com/@google/model-viewer/dist/model-viewer.min.js"></Script>
       <model-viewer id={styles.playingField} src="avia-23-playing-field.glb" ar ar-modes="webxr scene-viewer quick-look" camera-controls poster="poster.webp" shadow-intensity="1">
         <div class="progress-bar hide" slot="progress-bar">
             <div class="update-bar"></div>
