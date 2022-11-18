07cdd7813a88e1f0ff3e9b02b5c1ebf408d29681
cw118 <carolyn94118@gmail.com>
2022-11-18 10:43:35 -0500

    Correct to className attribute

--- components/GameField.js
+++ components/GameField.js
@@ -1,15 +1,16 @@
 // import styles from '../styles/<>.module.css';
+// add id={styles.gameField} to <model-viewer> once the CSS module stylesheet is linked
 import Script from 'next/script'
 
 const GameField = () => {
   return (
     <>
       <Script type="module" src="https://unpkg.com/@google/model-viewer/dist/model-viewer.min.js"></Script>
-      <model-viewer id={styles.playingField} src="avia-23-playing-field.glb" ar ar-modes="webxr scene-viewer quick-look" camera-controls poster="poster.webp" shadow-intensity="1">
-        <div class="progress-bar hide" slot="progress-bar">
-            <div class="update-bar"></div>
+      <model-viewer src="avia-23-playing-field.glb" ar ar-modes="webxr scene-viewer quick-look" camera-controls poster="poster.webp" shadow-intensity="1">
+        <div className="progress-bar hide" slot="progress-bar">
+          <div className="update-bar"></div>
         </div>
-        </model-viewer>
+      </model-viewer>
     </>
   )
 }
