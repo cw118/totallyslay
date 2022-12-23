ff122133e491750d5ebb8cdb45c74313a47ecbd0
cw118 <carolyn94118@gmail.com>
2022-12-22 21:14:48 -0500

    refactor(idScan.js): use framer-motion for animation

--- components/IdScan.js
+++ components/IdScan.js
@@ -1,28 +1,13 @@
-import { useState } from 'react';
 import styles from '../styles/Scantest.module.css';
+import { motion as m } from 'framer-motion';
 
 const IdScan = () => {
-  const [mouseDown, setMouseDown] = useState(false);
-  const [scanComplete, setScanComplete] = useState(false);
-
-  const playScan = () => {
-    setMouseDown(true);
-  }
-
-  const pauseScan = () => {
-    setMouseDown(false);
-  }
-
-  const validate = () => {
-    setScanComplete(true);
-  }
 
   return (
-    <div className={styles.container} onMouseDown={playScan} onTouchStart={playScan} onMouseUp={pauseScan} onTouchEnd={pauseScan}>
-      <div className={styles.scanCursor}></div>
-      <div className={styles.idScan}>
-        <div className={styles.idUnverified + (scanComplete ? `` : '')} id={styles.scanPaused + (mouseDown ? `${styles.scan}` : '')} onAnimationEnd={validate}></div>
-      </div>
+    <div className={styles.container}>
+      <m.div className={styles.idUnverified} initial={{ animationPlayState: 'paused' }} whileTap={{ animationPlayState: 'running' }}>
+        <m.div className={styles.scanCursor}></m.div>
+      </m.div>
     </div>
   )
 }
