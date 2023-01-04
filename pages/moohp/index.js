239cea009469f45f0101caa970bfb1874e819ca4
cw118 <carolyn94118@gmail.com>
2023-01-04 11:22:36 -0500

    Complete initial spyNav

--- pages/moohp/index.js
+++ pages/moohp/index.js
@@ -1,5 +1,16 @@
-const moohp = () => {
+import Puzzle from '../../components/Puzzle';
+import SpyNav from '../../components/SpyNav';
+import styles from '../../styles/Moohp.module.css';
 
+const moohp = () => {
+  return (
+    <>
+      <SpyNav />
+      <div className={styles.container}>
+        <Puzzle />
+      </div>
+    </>
+  );
 }
 
 export default moohp;
