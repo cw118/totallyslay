a741b132fb5455e5d18ee0c48944dea24f855409
cw118 <carolyn94118@gmail.com>
2023-01-06 22:16:07 -0500

    Add Meta for students page and fr text for pop-up

--- components/JerryPopUp.js
+++ components/JerryPopUp.js
@@ -12,7 +12,7 @@ const JerryPopUp = ({ lang }) => {
     <>
       <div className='jerryPopUp' onClick={handleShow}></div>
       <div className={'popUpTooltip font-spy' + (show ? '' : ' noOpacity')}>
-        <p>{lang ? '(traduction en français)' : 'Greetings agent, you have been recruited for a top-secret spy mission by the CRC!'} <Link href={lang ? '/fr/moohp' : '/moohp'} className='popUpLink'>{lang ? 'Cliquez/appuyez ici pour commencer.' : 'Click/tap here to begin.'}</Link></p>
+        <p>{lang ? 'Cher agent, vous avez été recruté pour une mission d\'espionnage ultra confidentielle par la CRC! ' : 'Greetings agent, you have been recruited for a top-secret spy mission by the CRC!'} <Link href={lang ? '/fr/moohp' : '/moohp'} className='popUpLink'>{lang ? 'Cliquez/tapez ici pour commencer.' : 'Click/tap here to begin.'}</Link></p>
       </div>
     </>
   );
