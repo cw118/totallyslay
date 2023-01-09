8beea025aa86b333cfa0faad8eb84bdc035d6810
cw118 <carolyn94118@gmail.com>
2023-01-09 14:33:49 -0500

    Complete initial spy home pages

---
+++ components/home/MoohpHero.js
@@ -0,0 +1,11 @@
+import styles from '../../styles/Moohp.module.css';
+
+const MoohpHero = () => {
+  return (
+    <div className={styles.hero}>
+      <img src='/logo/moohp-logo-alt.svg' alt='MOOHP' />
+    </div>
+  );
+}
+
+export default MoohpHero;
