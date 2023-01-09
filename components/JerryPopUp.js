955984576db9ce018f0eb6348b66105844431971
cw118 <carolyn94118@gmail.com>
2023-01-08 19:22:35 -0500

    Complete Jerry pop-up and unrelated nitpicking

--- components/JerryPopUp.js
+++ components/JerryPopUp.js
@@ -10,8 +10,8 @@ const JerryPopUp = ({ lang }) => {
 
   return (
     <>
-      <div className='jerryPopUp' onClick={handleShow}></div>
-      <div className={'popUpTooltip font-spy' + (show ? '' : ' noOpacity')}>
+      <div className={'jerryPopUp' + (show ? ' activePopUp' : '')} onClick={handleShow}></div>
+      <div className={'popUpTooltip font-spy' + (show ? '' : ' noOpacity invisible')}>
         <p>{lang ? 'Cher agent, vous avez été recruté pour une mission d\'espionnage ultra confidentielle par la CRC! ' : 'Greetings agent, you have been recruited for a top-secret spy mission by the CRC!'} <Link href={lang ? '/fr/moohp' : '/moohp'} className='popUpLink'>{lang ? 'Cliquez/tapez ici pour commencer.' : 'Click/tap here to begin.'}</Link></p>
       </div>
     </>
