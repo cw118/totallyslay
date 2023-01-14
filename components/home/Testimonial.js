4b93e879d8c5fe1bbd651783fd51327df8afe407
cw118 <carolyn94118@gmail.com>
2023-01-14 12:44:24 -0500

    Typofixes and updated translations/journal

--- components/home/Testimonial.js
+++ components/home/Testimonial.js
@@ -1,11 +1,12 @@
 import styles from '../../styles/Home.module.css';
+import Link from 'next/link';
 
 const Testimonial = ({ fr }) => {
     return(
         <div className={styles.testimonialContainer}>
             <div className={styles.testText}>
                 <p className={styles.quote}>{fr ? 'Je serai toujours reconnaissant envers l’Institut de recherche académique de Marianopolis pour encourager l’interdisciplinarité. L’institut encourage fortement ses élèves à participer aux activités parascolaires offertes et d’après moi, ceci est la principale raison pour le succès de ses élèves. Les valeurs que j’ai apprises pendant mes années à M.A.R.I. ont influencé la manière dont je gère MOOHP.' : 'I am forever grateful for the Marianopolis Academic Research Institute for always embracing the intersection of various fields. I believe that the institutes push for extracurricular activities is at the core of the success of its student body. The values and ethics I\'ve adopted from my years at M.A.R.I. highly influence how I run MOOHP today.'} </p>
-                <p className={styles.name}>{fr ? 'Jared Lewis, ancien élève de M.A.R.I. et fondateur de MOOHP' : 'Jared Lewis, M.A.R.I. Alumni & MOOHP Founder'}</p>
+          <p className={styles.name}>{fr ? 'Jared Lewis, ancien élève de M.A.R.I. et ' : 'Jared Lewis, M.A.R.I. Alumni & '}<Link className={styles.hintLink} href={fr ? '/fr/moohp' : '/moohp'}>{fr ? 'fondateur de MOOHP' : 'MOOHP Founder'}</Link></p>
             </div>
             <img 
                 src="/testimonial/jared.svg"
--- components/journal/variablesPosts.js
+++ components/journal/variablesPosts.js
@@ -4,11 +4,11 @@ export const eng = [
     caption: 'Laa-Laa and Dipsy'
     },
     {author: 'Yau Hong Chau', date: 'January 6th, 2023', avatar:'/journal/avatars/yau.png', status: "offline", src: '/journal/post-images/camera.png',
-    user: "iwear_crocs", hashtags: 'filmclub', commenter: 'commenter', comment: 'comment', likes: 8,
+    user: "iwear_crocs", hashtags: 'filmclub', commenter: 'Victoria Hoang', comment: 'I guess that\'s her version of a mic drop', likes: 8,
     caption: 'Celia keeps hitting us with the microphone'
     },
     {author: 'Celia Shi', date: 'January 5th, 2023', avatar:'/journal/avatars/celia.png', status: "online", src: '/journal/post-images/school-pic.png',
-    user: "username", hashtags: 'HumbleByKendrickLamar', commenter: 'Carolyn Wu', comment: 'you look so happy though', likes: 5,
+    user: "username", hashtags: 'HumbleByKendrickLamar', commenter: 'Carolyn Wu', comment: 'You look so happy though', likes: 5,
     caption: 'I thought I was tall but they bumped me down to the middle row :(',
     },
     {author: 'Vincent Ly', date: 'January 5th, 2023', avatar:'/journal/avatars/vincent.png', status: "busy", src: '/journal/post-images/3spies.png',
@@ -16,24 +16,24 @@ export const eng = [
     caption: 'Night Vision sunglasses are locked and loaded.'
     },
     {author: 'Andrea Abou-Haidar', date: 'January 5th, 2023', avatar:'/journal/avatars/andrea.png', status: "online", src: '/journal/post-images/lunch.png',
-    user: "not_that_short", hashtags: 'hashtag', commenter: 'Sangeeta Vidyarthi', comment: 'OMG! Is this at MOOH-', likes: 'x',
+    user: "not_that_short", hashtags: 'topsecret', commenter: 'Sangeeta Vidyarthi', comment: 'OMG! Is this at MOOH-', likes: 19,
     caption: 'Lunch in a totally secret location after some top secret work ;)'
     },
     {author: 'Audrey Dumais', date: 'January 5th, 2023', avatar:'/journal/avatars/audrey.png', status: "offline", src: '/journal/post-images/spies.png',
-    user: "wall_hugger", hashtags: 'hashtag', commenter: 'commenter', comment: 'comment', likes: 'x',
-    caption: 'caption'
+    user: "wall_hugger", hashtags: 'totallyspies', commenter: 'Ruwa Qatmash', comment: 'Look at us in suits!', likes: 11,
+    caption: 'Spies with cheap sunglasses at your service'
     },
     {author: 'Sofia Del Raso', date: 'December 20th, 2022', avatar:'/journal/avatars/sofia.png', status: "online", src: '/journal/post-images/appa-funeral.png',
     user: "british_person123", hashtags: 'RIPAppa', commenter: 'Nehna Patel', comment: 'Who would do such a thing...', likes: 18,
     caption: 'Not a good mo(u)rning. He may stay in our hearts, but not our basement.'
     },
     {author: 'Elisa Xue', date: 'December 20th, 2022', avatar:'/journal/avatars/elisa.png', status: "away", src: '/journal/post-images/blond-yau.png',
-    user: "emu.lisa", hashtags: 'hashtag', commenter: 'commenter', comment: 'comment', likes: 'x',
-    caption: 'caption'
+    user: "emu.lisa", hashtags: 'funhaircuts', commenter: 'Adelina Badea', comment: 'My dream hairstyle', likes: 17,
+    caption: 'A visit to my favourite hair salon'
     },
     {author: 'Isabella Hensley', date: 'November 16th, 2022', avatar:'/journal/avatars/isabella.png', status: "offline", src: '/journal/post-images/baldcap.png',
-    user: "british_person123", hashtags: 'hashtag', commenter: 'commenter', comment: 'comment', likes: 'x',
-    caption: 'caption'
+    user: "british_person123", hashtags: 'bald', commenter: 'Yau Hong Chau', comment: 'You of course', likes: 7,
+    caption: 'What are you looking at?'
     },
     {author: 'Isabella Hensley', date: 'November 13th, 2022', avatar:'/journal/avatars/isabella.png', status: "offline", src: '/journal/post-images/cabbage.png',
     user: "british_person123", hashtags: 'vegan', commenter: 'Sofia Del Raso', comment: 'I won BTW!!', likes: 15,
@@ -47,7 +47,7 @@ export const fr = [
     caption: 'Laa-Laa et Dipsy'
     },
     {author: 'Yau Hong Chau', date: '6 janvier 2023', avatar:'/journal/avatars/yau.png', status: "offline", src: '/journal/post-images/camera.png',
-    user: "iwear_crocs", hashtags: 'clubdefilm', commenter: 'commenter', comment: 'comment', likes: 8,
+    user: "iwear_crocs", hashtags: 'clubdefilm', commenter: 'Victoria Hoang', comment: 'C\'est probablement sa façon de faire un "mic drop"', likes: 8,
     caption: 'Celia n\'arrête pas de nous frapper avec le microphone'
     },
     {author: 'Celia Shi', date: 'January 5th, 2023', avatar:'/journal/avatars/celia.png', status: "online", src: '/journal/post-images/school-pic.png',
@@ -59,27 +59,27 @@ export const fr = [
     caption: 'Les lunettes à vision nocturne sont verrouillées et chargées.'
     },
     {author: 'Andrea Abou-Haidar', date: 'January 5th, 2023', avatar:'/journal/avatars/', status: "online", src: '/journal/post-images/lunch.png',
-    user: "acrobatics_IDK WHAT TO PUT", hashtags: 'hashtag', commenter: 'Sangeeta Vidyarthi', comment: 'OMG! Is this at MOOH-', likes: 'x',
+    user: "acrobatics_IDK WHAT TO PUT", hashtags: 'topsecret', commenter: 'Sangeeta Vidyarthi', comment: 'Oh mon dieu, est-ce à MOOH-', likes: 19,
     caption: 'Le dîner dans un endroit secret après du travail top secret ;)'
     },
-    {author: 'Isabella Hensley', date: 'January 5th, 2023', avatar:'/journal/avatars/isabella.png', status: "offline", src: '/journal/post-images/spies.png',
-    user: "british_person123", hashtags: 'hashtag', commenter: 'commenter', comment: 'comment', likes: 'x',
-    caption: 'caption'
+    {author: 'Audrey Dumais', date: 'January 5th, 2023', avatar:'/journal/avatars/audrey.png', status: "offline", src: '/journal/post-images/spies.png',
+    user: "wall_hugger", hashtags: 'TotalementDesEspions', commenter: 'Ruwa Qatmash', comment: 'Comme nous sommes élégantes en complet!', likes: 11,
+    caption: 'Des espions dotés de lunettes de soleil en plastique à votre service'
     },
     {author: 'Sofia Del Raso', date: 'December 20th, 2022', avatar:'/journal/avatars/sofia.png', status: "online", src: '/journal/post-images/appa-funeral.png',
-    user: "s.a.d.", hashtags: 'RIPAppa', commenter: 'Nehna Patel', comment: 'Qui ferais une telle chose...', likes: 18,
+    user: "s.a.d.", hashtags: 'RIPAppa', commenter: 'Nehna Patel', comment: 'Qui ferait une telle chose...', likes: 18,
     caption: 'Pas un bon matin. Il restera dans nos coeurs, mais pas dans notre espace de travail.'
     },
     {author: 'Elisa Xue', date: 'December 20th, 2022', avatar:'/journal/avatars/elisa.png', status: "away", src: '/journal/post-images/blond-yau.png',
-    user: "emu.lisa", hashtags: 'hashtag', commenter: 'commenter', comment: 'comment', likes: 'x',
-    caption: 'caption'
+    user: "emu.lisa", hashtags: 'coiffuresAmusantes', commenter: 'Adelina Badea', comment: 'La coiffure de mes rêves', likes: 17,
+      caption: 'Un tour à mon salon de coiffure préféré :D'
     },
     {author: 'Isabella Hensley', date: '16 novembre 2022', avatar:'/journal/avatars/isabella.png', status: "offline", src: '/journal/post-images/baldcap.png',
-    user: "british_person123", hashtags: 'hashtag', commenter: 'commenter', comment: 'comment', likes: 'x',
-    caption: 'caption'
+    user: "british_person123", hashtags: 'chauve', commenter: 'Yau Hong Chau', comment: 'Toi évidemment', likes: 7,
+    caption: 'Qu\'est-ce que tu regardes, toi?'
     },
     {author: 'Isabella Hensley', date: 'November 13th, 2022', avatar:'/journal/avatars/isabella.png', status: "offline", src: '/journal/post-images/cabbage.png',
-    user: "british_person123", hashtags: 'vegane', commenter: 'Sofia Del Raso', comment: 'C\'est moi qui a gagné!!', likes: 15,
+    user: "british_person123", hashtags: 'végane', commenter: 'Sofia Del Raso', comment: 'C\'est moi qui a gagné!!', likes: 15,
     caption: 'J\'ai gagné des billets au premier rang à un match de chouxballe! (Andrea a gagné).'
     }
 ]
